3c7d1768d88d30fb30b4e83c2dacba10e9e0ec88
duyquangbui99 <duyquangbui99@gmail.com>
2025-04-01 15:26:45 -0500

    open and close sidebar feature on mobile screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@
 import React from "react";
 import menuIcon from '../assets/images/menu.svg';
 
-const Navbar = () => {
+const Navbar = ({ toggleSidebar }) => {
     return (
         <div className="top-navbar">
-            <div className="open-menu" id="open-menu">
+            <div className="open-menu" id="open-menu" onClick={toggleSidebar}>
                 <img src={menuIcon} alt="menu Logo" className="menu-icon" />
             </div>
             <div className="menu-text">
