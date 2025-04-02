f02214db15f24218aa8485b3e63c975c1886e478
duyquangbui99 <duyquangbui99@gmail.com>
2025-04-01 19:59:35 -0500

    add experience section

---
+++ src/assets/expData.js
@@ -0,0 +1,26 @@
+const expData = [
+    {
+        position: "Software Development Intern",
+        company: "American Fidelity",
+        location: "OKC, OK",
+        date: "Jan 2025 – Present",
+        details: [
+            "Assisted in the full-stack development of the American Fidelity internal application, optimizing performance and improving production efficiency, reducing page load times by 25%.",
+            "Designed and implemented new features, models, and responsive UI components using C#, .NET, Angular, Bootstrap 5, HTML, and SASS, enhancing user experience and increasing accessibility by 30%.",
+            "Collaborated with QA and development teams to test, refine, and deploy updates across multiple environments using Octopus Deploy, reducing deployment errors by 40% and improving release efficiency."
+        ]
+    },
+    {
+        position: "Web Developer",
+        company: "Tranquility Nail and Spa",
+        location: "Tulsa, OK",
+        date: "June 2023 - May 2024",
+        details: [
+            "Developed a customer feedback app with a team member using the MERN stack (MongoDB, Express.js, React.js, Node.js), enabling efficient customer-employee communication.",
+            "Designed and developed the front-end application using React.js, implementing responsive UI components and integrating with back-end APIs built using Node.js and Express.js to manage user authentication and customer feedback, enhancing customer satisfaction by 20%.",
+            "Implemented manager-employee communication features, leading to a 15% improvement in employee performance and customer retention."
+        ]
+    }
+];
+
+export default expData;
